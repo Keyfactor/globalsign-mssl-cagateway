81fba8f8865ff576f0e7f10a68f752b6c4041c96
David Galey <dgaley@keyfactor.com>
2022-04-13 13:13:11 -0400

    Convert GS order status to Keyfactor enum

--- src/GlobalSignCAProxy/Api/GlobalSignOrderStatus.cs
+++ src/GlobalSignCAProxy/Api/GlobalSignOrderStatus.cs
@@ -5,19 +5,54 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
 // and limitations under the License.
 
+using CSS.PKI;
+
 namespace Keyfactor.Extensions.AnyGateway.GlobalSign.Api
 {
-    public enum GlobalSignOrderStatus
-    {
-        Initial = 1,
-        Waiting = 2,
-        Canceled = 3,
-        Issued = 4,
-        Cancelled = 5,
-        Revoking = 6,
-        Revoked = 7,
-        PendingApproval = 8,
-        Locked = 9,
-        Denied = 10
-    }
+	public enum GlobalSignOrderStatus
+	{
+		Initial = 1,
+		Waiting = 2,
+		Canceled = 3,
+		Issued = 4,
+		Cancelled = 5,
+		Revoking = 6,
+		Revoked = 7,
+		PendingApproval = 8,
+		Locked = 9,
+		Denied = 10
+	}
+
+	public static class OrderStatus
+	{
+		public static int ConvertToKeyfactorStatus(GlobalSignOrderStatus status)
+		{
+			switch (status)
+			{
+				case GlobalSignOrderStatus.Issued:
+					return (int)PKIConstants.Microsoft.RequestDisposition.ISSUED;
+
+				case GlobalSignOrderStatus.Revoked:
+				case GlobalSignOrderStatus.Revoking:
+					return (int)PKIConstants.Microsoft.RequestDisposition.REVOKED;
+
+				case GlobalSignOrderStatus.PendingApproval:
+				case GlobalSignOrderStatus.Waiting:
+					return (int)PKIConstants.Microsoft.RequestDisposition.EXTERNAL_VALIDATION;
+
+				case GlobalSignOrderStatus.Initial:
+					return (int)PKIConstants.Microsoft.RequestDisposition.IN_PROCESS;
+
+				case GlobalSignOrderStatus.Denied:
+					return (int)PKIConstants.Microsoft.RequestDisposition.DENIED;
+
+				case GlobalSignOrderStatus.Canceled:
+				case GlobalSignOrderStatus.Cancelled:
+					return (int)PKIConstants.Microsoft.RequestDisposition.FAILED;
+
+				default:
+					return (int)PKIConstants.Microsoft.RequestDisposition.UNKNOWN;
+			}
+		}
+	}
 }
